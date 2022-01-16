b98e3995a33848ad63c0a8daab07ca88d7d9cf8e
BURGOS LOMBARDI Ezequiel <ezequiel.burgos@soprasteria.com>
2022-01-16 11:10:42 +0100

    test(backend): added video fixtures

---
+++ backend/fixtures/video.js
@@ -0,0 +1,15 @@
+module.exports = {
+	data: {
+		_id: '557580900f2b07e0c39d23a0',
+		videoId: 'xA34kfi',
+		title: 'History of the Roman Empire',
+		author: 'National Geographic',
+		questions: [
+			{
+				_id: '823890ffdbn',
+				time: 10,
+				text: 'What language was spoken in the Roman Empire?'
+			}
+		]
+	}
+}
---
+++ backend/fixtures/videos.js
@@ -0,0 +1,35 @@
+module.exports = {
+	data: [
+		{
+			_id: '557580900f2b07e0c39d23az',
+			videoId: 'xA34kfi',
+			title: 'History of the Roman Empire',
+			author: 'National Geographic',
+			questions: [
+				{
+					_id: '823890ffdbn',
+					time: 10,
+					text: 'What language was spoken in the Roman Empire?'
+				}
+			]
+		},
+		{
+			_id: '557580900f2b07e0c39d23a0',
+			videoId: 'iQWvc6qhTds',
+			title: 'Flipping the Classroom: Explained!',
+			author: 'Edpuzzle',
+			questions: [
+				{
+					_id: '56b9eb939b1b0aaf44f2147a',
+					time: 101,
+					text: 'How do you feel about flipping your classroom?'
+				},
+				{
+					_id: '56b9eb939b1b0aaf44f2147t',
+					time: 130,
+					text: 'How many years has Edpuzzle been around so far?'
+				}
+			],
+		}
+	]
+};
